0d4302931a9059cd1414709aa3373493f7b11d35
Shubham Singh <shubhams12333@gmail.com>
2025-07-09 15:20:23 +0530

    Handle contains lowercase letter

--- password-validation/app.py
+++ password-validation/app.py
@@ -1,10 +1,12 @@
 def is_password_valid(password:str)->bool:
     is_valid_len = len(password) > 8
  
-    has_capital_letter = False   
+    has_capital_letter = False  
+    has_lowercase_letter = False 
     for char in password:
         if char.isupper():
             has_capital_letter = True
-            break
+        else:
+            has_lowercase_letter = True
     
-    return is_valid_len and has_capital_letter
+    return is_valid_len and has_capital_letter and has_lowercase_letter
--- password-validation/test.py
+++ password-validation/test.py
@@ -7,3 +7,6 @@ def test_len_less_than_equal_to_eight():
 
 def test_does_not_contain_capital_letter():
     assert is_password_valid("lowercase") == False
+
+def test_does_not_contain_lowercase_letter():
+    assert is_password_valid("UPPERCASE") == False
