68fabb9617ed9a3f573533a0730a5507d2889d5d
Shubham Singh <shubhams12333@gmail.com>
2025-07-09 15:42:04 +0530

    refactor  password validator class

--- password-validation/app.py
+++ password-validation/app.py
@@ -2,22 +2,27 @@ class PasswordValidator:
     def __init__(self, password):
         self.password = password
 
-    def is_password_valid(self)->bool:
-        is_valid_len = len(self.password) > 8
+    def is_valid_length(self, length:int)->bool:
+        return len(self.password) > length
     
-        has_capital_letter = False  
-        has_lowercase_letter = False 
-        has_a_number = False
-        has_an_underscore = False
+    def has_capital_letter(self)->bool:
+        return any(char.isupper() for char in self.password)
+    
+    def has_lower_letter(self)->bool:
+        return any(char.islower() for char in self.password)
+    
+    def has_number(self)->bool:
+        return any(char.isdigit() for char in self.password)
+    
+    def has_underscore(self)->bool:
+        return "_" in self.password
+    
+    def is_password_valid(self)->bool:
 
-        for char in self.password:
-            if char.isupper():
-                has_capital_letter = True
-            elif char.islower():
-                has_lowercase_letter = True
-            elif char.isdigit():
-                has_a_number = True
-            elif char == "_":
-                has_an_underscore = True
-        
-        return is_valid_len and has_capital_letter and has_lowercase_letter and has_a_number and has_an_underscore
+        return (
+            self.is_valid_length(8) and
+            self.has_capital_letter() and 
+            self.has_lower_letter() and 
+            self.has_number() and 
+            self.has_underscore()
+                ) 
