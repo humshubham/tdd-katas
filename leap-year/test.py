2ca775325c9292a402ad28924974c6b99b26eddf
Shubham Singh <shubhams12333@gmail.com>
2025-07-09 14:45:01 +0530

    Add test cases for 4 and 400

--- leap-year/test.py
+++ leap-year/test.py
@@ -2,5 +2,14 @@ import pytest
 
 from app import is_leap_year
 
-def test_2001():
+def test_not_divisible_by_4():
     assert is_leap_year(2001) == False
+
+def test_divisible_by_4():
+    assert is_leap_year(2004) == True
+
+def test_not_divisible_by_400():
+    assert is_leap_year(1967) == False
+
+def test_divisible_by_400():
+    assert is_leap_year(1600) == True
