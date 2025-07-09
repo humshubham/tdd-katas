c1f05ebbefc6f62fb76dde658620e0a090f64a6e
Shubham Singh <shubhams12333@gmail.com>
2025-07-09 14:09:56 +0530

    handle 2

--- fizz-buzz/test.py
+++ fizz-buzz/test.py
@@ -5,5 +5,8 @@ from app import convert
 def test_convert_one():
     assert convert(1) == "1"
 
-def test_convert_one():
+def test_convert_two():
     assert convert(2) == "2"
+
+def test_convert_three():
+    assert convert(3) == "Fizz"
