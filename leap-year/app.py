d86946af426134d53b006b0c567d0551db2c1776
Shubham Singh <shubhams12333@gmail.com>
2025-07-09 15:01:25 +0530

    Handle divisible by 100 and not by 400 test cases

--- leap-year/app.py
+++ leap-year/app.py
@@ -1,2 +1,6 @@
 def is_leap_year(year:int) -> bool:
+    if year%100 == 0:
+        if year%400 != 0:
+            return False
+        return True
     return year % 4 == 0
--- leap-year/test.py
+++ leap-year/test.py
@@ -13,3 +13,9 @@ def test_not_divisible_by_400():
 
 def test_divisible_by_400():
     assert is_leap_year(1600) == True
+
+def test_divisible_by_100_not_400():
+    assert is_leap_year(1800) == False
+
+def test_divisible_by_100_and_400():
+    assert is_leap_year(2000) == True
