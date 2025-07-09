ebd1b7c734df5567bf5e1182560ab3d980bbd29b
Shubham Singh <shubhams12333@gmail.com>
2025-07-09 12:48:24 +0530

    handle 2

--- prime-factors/app.py
+++ prime-factors/app.py
@@ -1,2 +1,4 @@
 def prime_factors(n:int):
+    if n>1:
+        return [n]
     return []
--- prime-factors/test.py
+++ prime-factors/test.py
@@ -4,3 +4,6 @@ from app import prime_factors
 
 def test_factors_of_one():
     assert(prime_factors(1)) == []
+
+def test_factors_of_two():
+    assert(prime_factors(2)) == [2]
