f0a2e8a5769d6e8b36b7da9642e00a4cd73cb1bf
Shubham Singh <shubhams12333@gmail.com>
2025-07-10 10:57:09 +0530

    handle day 1

--- tweleve-christmas-days/app.py
+++ tweleve-christmas-days/app.py
@@ -1,4 +1,4 @@
 def print_christmas_song(day:int)->str:
     if day < 1 or day > 12:
         raise Exception("Day should be from 1 to 12")
-    return "hello"
+    return "On the First day of Christmas,\nMy true love gave to me:\nA partridge in a pear tree"
--- tweleve-christmas-days/test.py
+++ tweleve-christmas-days/test.py
@@ -11,3 +11,6 @@ def test_more_than_twelve():
     with pytest.raises(Exception) as e:
         print_christmas_song(13)
     assert "Day should be from 1 to 12" in str(e)
+
+def test_day_one():
+    assert   print_christmas_song(1) == "On the First day of Christmas,\nMy true love gave to me:\nA partridge in a pear tree"
