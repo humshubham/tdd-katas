79bcfadb8bc912b2e54a3783f58327088626d2a5
Shubham Singh <shubhams12333@gmail.com>
2025-07-10 10:46:58 +0530

    handle larger integers

--- christmas-tree/app.py
+++ christmas-tree/app.py
@@ -2,9 +2,19 @@ def print_christmas_tree(height:int)->str:
     if height < 2:
         raise Exception("Height can not be less than two")
     christmas_tree_str = ""
-    
-    christmas_tree_str+= " X\n"
-    christmas_tree_str+="XXX\n"
-    christmas_tree_str+=" |"
+
+    leading_whitespace = height-1
+    tree_elements = 1
+
+    while leading_whitespace>=0:
+        christmas_tree_str+= " "*leading_whitespace
+        christmas_tree_str+="X"*tree_elements
+        christmas_tree_str+="\n"
+        tree_elements+=2
+        leading_whitespace-=1
+
+    #Tree trunk
+    christmas_tree_str+=" "*(height-1)
+    christmas_tree_str+="|"
+
     return christmas_tree_str
-    
--- christmas-tree/test.py
+++ christmas-tree/test.py
@@ -14,3 +14,12 @@ def test_height_one():
 
 def test_height_two():
     assert print_christmas_tree(2) == """ X\nXXX\n |"""
+
+def test_height_three():
+    assert print_christmas_tree(3) == """  X\n XXX\nXXXXX\n  |"""
+
+def test_height_four():
+    assert print_christmas_tree(4) == """   X\n  XXX\n XXXXX\nXXXXXXX\n   |"""
+
+def test_height_ten():
+    assert print_christmas_tree(10) == """         X\n        XXX\n       XXXXX\n      XXXXXXX\n     XXXXXXXXX\n    XXXXXXXXXXX\n   XXXXXXXXXXXXX\n  XXXXXXXXXXXXXXX\n XXXXXXXXXXXXXXXXX\nXXXXXXXXXXXXXXXXXXX\n         |"""
