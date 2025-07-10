bcc10bd9a709aa80f6191f7107a573167f9be9a4
Shubham Singh <shubhams12333@gmail.com>
2025-07-10 11:36:09 +0530

    handle all days

--- tweleve-christmas-days/test.py
+++ tweleve-christmas-days/test.py
@@ -17,3 +17,9 @@ def test_day_one():
 
 def test_day_two():
     assert print_christmas_song(2).lower() == "On the Second day of Christmas,\nMy true love gave to me:\nTwo turtle doves,\nAnd a partridge in a pear tree.".lower()
+
+def test_day_five():
+    assert print_christmas_song(5).lower() == "On the fifth day of Christmas,\nMy true love gave to me:\nFive golden rings,\nFour calling birds,\nThree French hens,\nTwo turtle doves,\nAnd a partridge in a pear tree.".lower()
+
+def test_day_twelve():
+    assert print_christmas_song(12).lower() == "On the twelfth day of Christmas,\nMy true love gave to me:\nTwelve drummers drumming,\nEleven pipers piping,\nTen lords a-leaping,\nNine ladies dancing,\nEight maids a-milking,\nSeven swans a-swimming,\nSix geese a-laying,\nFive golden rings,\nFour calling birds,\nThree French hens,\nTwo turtle doves,\nAnd a partridge in a pear tree.".lower()
